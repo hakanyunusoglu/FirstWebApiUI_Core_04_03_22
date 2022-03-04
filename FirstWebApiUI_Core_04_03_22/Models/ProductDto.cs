8d97ff48e62529920e2d1343d034bb9fc845349b
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-03-04 09:30:03 +0300

    Add project files.

---
+++ FirstWebApiUI_Core_04_03_22/Models/ProductDto.cs
@@ -0,0 +1,9 @@
+﻿namespace FirstWebApiUI.Models
+{
+    public class ProductDto
+    {
+        public int ProductID { get; set; }
+        public string Description { get; set; }
+        public string Brand { get; set; }
+    }
+}
---
+++ FirstWebApiUI_Core_04_03_22/wwwroot/js/Product.js
@@ -0,0 +1,11 @@
+﻿function LoadProductByApi() {
+    $.ajax({
+        type: "GET",
+        url: "https://localhost:44315/api/Product/LoadProductList",
+        dataType: "json",
+        contentType: "application/json; charset=UTF-8",
+        success: function (returndata) {
+
+        }
+    })
+}
