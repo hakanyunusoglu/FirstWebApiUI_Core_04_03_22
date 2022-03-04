a9a73d55d64fa9e3d1f5a342f121e87c242624da
hakanyunusoglu <hakanyunusoglu93@gmail.com>
2022-03-04 11:42:04 +0300

    ghjgjgjgjg

--- FirstWebApiUI_Core_04_03_22/Controllers/HomeController.cs
+++ FirstWebApiUI_Core_04_03_22/Controllers/HomeController.cs
@@ -1,10 +1,13 @@
-﻿using FirstWebApiUI_Core_04_03_22.Models;
+﻿using FirstWebApiUI.Models;
+using FirstWebApiUI_Core_04_03_22.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Linq;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace FirstWebApiUI_Core_04_03_22.Controllers
@@ -19,13 +22,29 @@ namespace FirstWebApiUI_Core_04_03_22.Controllers
         }
 
         public IActionResult Index()
-        {
+        {           
             return View();
         }
 
         public IActionResult Privacy()
         {
-            return View();
+            List<ProductDto> resultlist = new List<ProductDto>();
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri("https://localhost:44315/api/");
+                var responseTalk = client.GetAsync("Product/LoadProductList");
+                responseTalk.Wait();
+
+                var result = responseTalk.Result;
+                if (result.IsSuccessStatusCode)
+                {
+                    var readTask = result.Content.ReadAsStringAsync();
+                    readTask.Wait();
+                    var products = readTask.Result;
+                    resultlist = JsonConvert.DeserializeObject<List<ProductDto>>(products);
+                }
+            }
+            return View(resultlist);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
